df4ed487daa415fc6ffd45c8538ba11c3818599d
Piotr Wysocki <wysockip14@gmail.com>
2026-05-21 20:41:05 +0200

    list extensions - shuffle method

---
+++ Packages/pl.nesur.core/Runtime/Util/ListExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Nesur.Core.Util {
+    public static class ListExtensions {
+        public static void Shuffle<T>(this IList<T> list) {
+            int n = list.Count;
+            while (n > 1) {
+                n--;
+                int k = Random.Range(0, n + 1);
+                (list[k], list[n]) = (list[n], list[k]);
+            }
+        }
+    }
+}
