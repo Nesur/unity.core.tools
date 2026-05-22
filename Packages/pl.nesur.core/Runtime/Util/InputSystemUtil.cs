da20260279182dc4343e16220dbb2d81fbc94b75
Piotr Wysocki <wysockip14@gmail.com>
2026-05-22 20:14:19 +0200

    - added InputSystemUtil.csm

---
+++ Packages/pl.nesur.core/Runtime/Util/InputSystemUtil.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Nesur.Core.Util {
+    /// <summary>
+    /// New input system utility class
+    /// </summary>
+    public class InputSystemUtil {
+        public static bool WasEscapeReleased() {
+            if (Keyboard.current != null) {
+                return Keyboard.current.escapeKey.wasReleasedThisFrame;
+            }
+
+            Debug.Log("No keyboard found. Cannot check if Escape key was released.");
+            return false;
+        }
+        public static bool WasEscapePressed() {
+            if (Keyboard.current != null) {
+                return Keyboard.current.escapeKey.wasPressedThisFrame;
+            }
+
+            Debug.Log("No keyboard found. Cannot check if Escape key was pressed.");
+            return false;
+        }
+    }
+}
