f85f5107f2f9da819d9dd864c8726df905b3b26e
Piotr Wysocki <wysockip14@gmail.com>
2026-06-02 20:47:38 +0200

    refactor JsonSaveSystem and ISaveSystem to use saveName instead of path for improved clarity and added GetSaveFilePath method

--- Packages/pl.nesur.core/Runtime/System/Save/ISaveSystem.cs
+++ Packages/pl.nesur.core/Runtime/System/Save/ISaveSystem.cs
@@ -1,6 +1,6 @@
 ﻿namespace Nesur.Core.System.Save {
     public interface ISaveSystem<T> {
-        void Save(T saveObject, string path);
-        T Load(string path);
+        void Save(T saveObject, string saveName);
+        T Load(string saveName);
     }
 }
--- Packages/pl.nesur.core/Runtime/System/Save/JsonSaveSystem.cs
+++ Packages/pl.nesur.core/Runtime/System/Save/JsonSaveSystem.cs
@@ -4,19 +4,24 @@ using UnityEngine;
 
 namespace Nesur.Core.System.Save {
     public class JsonSaveSystem<T> : ISaveSystem<T> {
-        public void Save(T saveObject, string path) {
+        public void Save(T saveObject, string saveName) {
             string json = JsonUtility.ToJson(saveObject);
             byte[] encryptedJson = EncryptionUtil.EncryptUsingAes(json);
-            File.WriteAllBytes(path, encryptedJson);
+            string saveFilePath = GetSaveFilePath(saveName);
+            File.WriteAllBytes(saveFilePath, encryptedJson);
         }
 
-        public T Load(string path) {
-            if (File.Exists(path)) {
-                byte[] encryptedJson = File.ReadAllBytes(path);
+        public T Load(string saveName) {
+            string saveFilePath = GetSaveFilePath(saveName);
+            if (File.Exists(saveFilePath)) {
+                byte[] encryptedJson = File.ReadAllBytes(saveFilePath);
                 string json = EncryptionUtil.DecryptUsingAes(encryptedJson);
                 return JsonUtility.FromJson<T>(json);
             }
             return default(T);
+        }
+        private string GetSaveFilePath(string saveName) {
+            return Application.persistentDataPath + "/" + saveName;
         }
     }
 }
