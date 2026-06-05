95586b157d32b9b6667eb6eb3c93d181d7ff722f
Piotr Wysocki <wysockip14@gmail.com>
2026-06-05 17:49:05 +0200

    add LookAtCamera component for camera alignment with optional up rotation lock

---
+++ Packages/pl.nesur.core/Runtime/LookAtCamera.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace Nesur.Core {
+    public class LookAtCamera : MonoBehaviour {
+        private static readonly Camera mainCamera = Camera.main;
+        [SerializeField] private bool lockUpRotation;
+
+        private void Update() {
+            Vector3 cameraForwardVector = mainCamera.transform.forward;
+            if (lockUpRotation) {
+                transform.forward = new Vector3(cameraForwardVector.x, 0, cameraForwardVector.z);
+            }
+            else {
+                transform.forward = cameraForwardVector;
+            }
+        }
+    }
+}
