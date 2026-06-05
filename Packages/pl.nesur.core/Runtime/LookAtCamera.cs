59e0b896e4abf028a5e86cfb3b0b6d697e63acc0
Piotr Wysocki <wysockip14@gmail.com>
2026-06-05 17:52:24 +0200

    refactor LookAtCamera to implement ITickable for improved alignment control and manage tick registration

--- Packages/pl.nesur.core/Runtime/LookAtCamera.cs
+++ Packages/pl.nesur.core/Runtime/LookAtCamera.cs
@@ -1,11 +1,31 @@
-﻿using UnityEngine;
+﻿using Nesur.Core.System;
+using UnityEngine;
+using NotImplementedException = System.NotImplementedException;
 
 namespace Nesur.Core {
-    public class LookAtCamera : MonoBehaviour {
+    public class LookAtCamera : MonoBehaviour, ITickable {
         private static readonly Camera mainCamera = Camera.main;
+        [SerializeField] private bool alignOnStart = true;
         [SerializeField] private bool lockUpRotation;
 
-        private void Update() {
+        public void OnTick() {
+            AlignWithCamera();
+        }
+
+        private void Start() {
+            if (alignOnStart) {
+                AlignWithCamera();
+            }
+            else {
+                TickManager.Instance.RegisterTickable(this);
+            }
+        }
+        
+        private void OnDestroy() {
+            TickManager.Instance.UnregisterTickable(this);
+        }
+
+        private void AlignWithCamera() {
             Vector3 cameraForwardVector = mainCamera.transform.forward;
             if (lockUpRotation) {
                 transform.forward = new Vector3(cameraForwardVector.x, 0, cameraForwardVector.z);
